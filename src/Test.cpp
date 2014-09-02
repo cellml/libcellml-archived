43965c82eae1e7371d8f7ac6a941d6af4dfcd693
Randall Britten <r.britten@auckland.ac.nz>
2014-09-03 11:34:32 +1200

    Dummy comment, just to test buildbot git repo polling, again.

--- src/Test.cpp
+++ src/Test.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 // Dummy comment, just to test buildbot git repo polling. Another fake change, same reason.
+// Another dummy change, buildbot test.
 int main() {
   cout << "Version " << libcellml::general::getVersion() << endl;
   return 0;
