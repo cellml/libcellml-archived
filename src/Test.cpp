c92664f3f46337edf8c82d5a1ea290178aa40fb6
Randall Britten <r.britten@auckland.ac.nz>
2014-09-03 19:08:58 +1200

    Deleting dummy comments which were for testing buildbot.

--- src/Test.cpp
+++ src/Test.cpp
@@ -3,8 +3,6 @@
 
 using namespace std;
 
-// Dummy comment, just to test buildbot git repo polling. Another fake change, same reason.
-// Another dummy change, buildbot test.
 int main() {
   cout << "Version " << libcellml::general::getVersion() << endl;
   return 0;
