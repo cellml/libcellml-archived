31721a099b5040eadee49570afe1d116c6acf272
Randall Britten <r.britten@auckland.ac.nz>
2014-09-26 15:15:49 +1200

    Make test pass.
    Also, follow convention of avoiding "using namespace std".

--- src/Version.cpp
+++ src/Version.cpp
@@ -3,14 +3,8 @@
 #include "Version.h"
 #include "LibcellmlConfig.h"
 
-using namespace std;
-
-static string badVersion;
-
-const string libcellml::general::getVersion() {
-    badVersion = badVersion + "... more junk";
-    ostringstream os;
-    os << LIBCELLML_VERSION_MAJOR << "." << LIBCELLML_VERSION_MINOR << endl;
-//    return os.str();
-    return badVersion;
+const std::string libcellml::general::getVersion() {
+    std::ostringstream os;
+    os << LIBCELLML_VERSION_MAJOR << "." << LIBCELLML_VERSION_MINOR << std::endl;
+    return os.str();
 }
