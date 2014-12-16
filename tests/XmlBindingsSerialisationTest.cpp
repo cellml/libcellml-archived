a5bb9cf12c5c5d68e24900421ebaeb27b0665e76
Randall Britten <r.britten@auckland.ac.nz>
2014-12-16 17:10:12 +1300

    Slightly improved XML serialisation test.
    Still much room for improvement, it really is more of a demo at the moment.

--- tests/XmlBindingsSerialisationTest.cpp
+++ tests/XmlBindingsSerialisationTest.cpp
@@ -1,5 +1,7 @@
 #include <memory>
 #include <iostream>
+#include <strstream>
+#include <string>
 
 #include "cellml_1_2.hxx"
 #include "gtest/gtest.h"
@@ -7,9 +9,22 @@
 using namespace std;
 using namespace cellml12;
 
-//! Test creation of empty model using manager
-TEST(XmlBindingsSerialisation, DummyXmlOutput) {
-  cellml12::Model m("blah");
+string xml1() {
+  return
+    "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\" ?>\n"
+    "<p1:model xmlns:p1=\"http://www.cellml.org/cellml/1.2#\" name=\"modelName\">\n"
+    "  <p1:component name=\"c1\">\n"
+    "    <p1:variable name=\"v1\" public_interface=\"yes\" type=\"real\" units=\"kg\"/>\n"
+    "  </p1:component>\n"
+    "  <p1:component name=\"c2\"/>\n"
+    "</p1:model>\n";
+}
+
+
+//! Test creation of a model and serialisation to XML
+TEST(XmlBindingsSerialisation, SimpleExample01) {
+  auto modelName = "modelName";
+  cellml12::Model m(modelName);
   cellml12::Component c1("c1");
   cellml12::Variable v1("v1", "kg", "real");
 
@@ -20,16 +35,40 @@ TEST(XmlBindingsSerialisation, DummyXmlOutput) {
   cellml12::Component c2("c2");
   m.getComponent().push_back(c2);
 
-  serializeModel(cout, m);
+  ostringstream oss;
 
-  auto m2 = parseModel("example01.xml");
-  cout << endl;
-//  cout << "Read in model name: " << m2->name() << endl;
+  serializeModel(oss, m);
+  ASSERT_EQ(xml1(), oss.str());
 
-
-  ASSERT_EQ(true, true);
 }
 
+//! Test reading in a simple model from XML text.
+TEST(XmlBindingsDeserialisation, SimpleExample01) {
 
+  istringstream iss (xml1());
 
+  auto m2 = parseModel(iss, xml_schema::Flags::dont_validate);
 
+  ostringstream oss;
+  oss << "Read in model name: " << m2->getName() << endl;
+  oss << "  Components:" << endl;
+  auto cs = m2->getComponent();
+  for (auto c : cs) {
+    oss << "    " << c.getName() << endl;
+    auto vs = c.getVariable();
+    oss << "      Variables:" << endl;
+    for (auto v : vs) {
+      oss << "        " << v.getName() << "::" << v.getType() << "(private: " << v.getPrivate_interface() << ", public: " << v.getPublic_interface() << ")" << endl;
+    }
+  }
+
+  string expectedSummary(
+"Read in model name: modelName\n"
+"  Components:\n"
+"    c1\n"
+"      Variables:\n"
+"        v1::real(private: <not present>, public: yes)\n"
+"    c2\n"
+"      Variables:\n");
+  ASSERT_EQ(expectedSummary, oss.str());
+}
