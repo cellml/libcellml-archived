773a55153f676f87143958a09603f582d55a40c2
Randall Britten <r.britten@auckland.ac.nz>
2014-12-18 11:00:02 +1300

    Travis: Try using default path for cmake install

--- .travis_scripts/cmake.sh
+++ .travis_scripts/cmake.sh
@@ -5,7 +5,7 @@ wget http://www.cmake.org/files/v3.1/cmake-3.1.0.tar.gz
 tar xzf cmake-3.1.0.tar.gz
 cd cmake-3.1.0
 cmake .
-./configure --prefix=/opt/cmake
+./configure
 make
 
 sudo apt-get remove cmake cmake-data
