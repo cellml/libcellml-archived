8ab4690a90e841400317aee147cab271646c7e49
Randall Britten <r.britten@auckland.ac.nz>
2014-12-19 11:56:30 +1300

    Added UnitTest unit test, intentionally set to fail.

---
+++ tests/UnitTest.cpp
@@ -0,0 +1,18 @@
+#include <memory>
+#include <iostream>
+#include "UnitsOwner.h"
+#include "Unit.h"
+
+#include "gtest/gtest.h"
+
+using namespace libcellml::model;
+using namespace std;
+
+//! Test unit has name specified during creation
+TEST(Unit, UnitNaming) {
+  shared_ptr<UnitsOwner> uo = make_shared<UnitsOwner>(); // todo: owner shouldn't float, should be Model or Component.
+  auto u1 = uo->createUnit(L"kg");
+
+  ASSERT_EQ(u1->getName(), L"force test to fail");
+}
+
