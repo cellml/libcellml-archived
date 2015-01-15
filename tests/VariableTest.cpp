ca09100c8d25ca59103f07fbfd08f5e77aee6d35
Randall Britten <r.britten@auckland.ac.nz>
2015-01-15 15:54:02 +1300

    Added a simple variable connection test.

--- tests/VariableTest.cpp
+++ tests/VariableTest.cpp
@@ -24,3 +24,23 @@ TEST(Variable, VariableSelfConnection) {
 
   ASSERT_EQ(1, connectedVars.size());
 }
+
+
+/**! Test a simple connection between to variables from separate components.
+ */
+TEST(Variable, SimpleVariableConnection) {
+  shared_ptr<Manager> m = make_shared<Manager>();
+  auto m1 = m->createModel(L"test model");
+  auto c1 = m1->createComponent(L"test component 1");
+  auto c2 = m1->createComponent(L"test component 2");
+  auto u1 = m1->createUnit(L"kg");
+  auto v1 = c1->createVariable(L"v1", u1);
+  auto v2 = c2->createVariable(L"v2", u1);
+  v1->connect(v2);
+
+  auto v1ConnectedVars = v1->getConnectedVariables();
+  ASSERT_EQ(1, v1ConnectedVars.size());
+
+  auto v2ConnectedVars = v2->getConnectedVariables();
+  ASSERT_EQ(1, v2ConnectedVars.size());
+}
