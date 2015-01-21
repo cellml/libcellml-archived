728a6ad396eff81d7232766383409655e2222ecd
Randall Britten <r.britten@auckland.ac.nz>
2015-01-21 13:58:24 +1300

    Added test: serialise model where var has no unit.
    Test failing intentionally, next commit has required implementation.

--- tests/SerialiserTest.cpp
+++ tests/SerialiserTest.cpp
@@ -38,5 +38,28 @@ R"(<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
 }
 
 
+//! Test serialising model that has a variable that is missing units.
+TEST(Serialiser, xmlOutput_VarMissingUnits) {
+  shared_ptr<Manager> m = make_shared<Manager>();
+  auto m1 = m->createModel(L"test model");
+  auto c1 = m1->createComponent(L"test component");
+  auto v1 = c1->createVariable(L"test variable", boost::none);
+
+  Serialiser s;
+  auto xml = s.createXml(*m);
+
+  string expectedXml(
+R"(<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<p1:model xmlns:p1="http://www.cellml.org/cellml/1.2#" name="test model">
+  <p1:component name="test component">
+    <p1:variable name="test variable" type="real" units=""/>
+  </p1:component>
+</p1:model>
+)");
+
+  ASSERT_EQ(expectedXml, xml);
+}
+
+
 
 
