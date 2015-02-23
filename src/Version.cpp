4b69cec6957cbb2e9a9d0ffb6f7299813a8de8a1
Randall Britten <r.britten@auckland.ac.nz>
2015-02-23 16:20:11 +1300

    Revert "Break version implementation intentionally."
    This reverts commit bd4a32c5316181ba43e5da484aaba9705ed8d7c5.

--- src/Version.cpp
+++ src/Version.cpp
@@ -4,7 +4,7 @@
 #include "LibcellmlConfig.h"
 
 const std::string libcellml::getVersion() {
-    static std::ostringstream os;
+    std::ostringstream os;
     os << LIBCELLML_VERSION_MAJOR << "." << LIBCELLML_VERSION_MINOR << std::endl;
     return os.str();
 }
