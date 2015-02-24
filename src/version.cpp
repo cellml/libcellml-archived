1b452d9c9a5e6e4d87fba93bdaee5a3ea3d2f2ee
Randall Britten <r.britten@auckland.ac.nz>
2015-02-24 15:21:07 +1300

    Version patch level number added.

--- src/version.cpp
+++ src/version.cpp
@@ -5,6 +5,6 @@
 
 const std::string libcellml::getVersion() {
     std::ostringstream os;
-    os << LIBCELLML_VERSION_MAJOR << "." << LIBCELLML_VERSION_MINOR << std::endl;
+    os << LIBCELLML_VERSION_MAJOR << "." << LIBCELLML_VERSION_MINOR << "." << LIBCELLML_VERSION_PATCH << std::endl;
     return os.str();
 }
