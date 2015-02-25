1eb65e359fc429bcae5373a0181c5fb9f2285d71
Randall Britten <r.britten@auckland.ac.nz>
2015-02-25 15:32:28 +1300

    Elaborated on the version string dox.

--- src/version.h
+++ src/version.h
@@ -6,7 +6,9 @@
 //! Everything in LibCellML is in this namespace.
 namespace libcellml {
 
-/** Get the version string
+/** Get the version string.
+ * The version string is usually in the format x.y.z, where the "."s are literal, and x,y and z represent counting numbers,
+ * in which case x is the major version, y the minor version, and z the patch level.
  *  @return The version string
  */
 const std::string getVersion();
