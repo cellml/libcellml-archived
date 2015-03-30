c5aef139546bbe8a43b83a83309115f04eb3cea7
Randall Britten <r.britten@auckland.ac.nz>
2015-03-31 11:15:47 +1300

    Added Cmake flag to control whether warnings=errors.
    Default is that warnings are not treated as errors, however
    Buildbots will have this enabled.

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -1,10 +1,12 @@
 macro(INITIALISE_PROJECT)
-  if(WIN32)
-    set(CMAKE_CXX_FLAGS "/W3")
-  else()
-    set(CMAKE_CXX_FLAGS "-Wall -W -Werror")
+  if(LIBCELLML_WARNINGS_TREATED_AS_ERRORS)
+    if(WIN32)
+      set(CMAKE_CXX_FLAGS "/W3")
+    else()
+      set(CMAKE_CXX_FLAGS "-Wall -W -Werror")
+    endif()
   endif()
-
+  
   if (MSVC)
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd /EHsc")
   endif()
